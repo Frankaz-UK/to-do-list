9875be721add09dd6b4d8e515208961a6f8310a9
Frankaz <frankazx@gmail.com>
2024-07-16 14:16:23 +0100

    Tidy Up Layout

--- resources/js/components/TasksTable.vue
+++ resources/js/components/TasksTable.vue
@@ -1,6 +1,49 @@
 <template>
     <div>
-        <b-table striped hover :items="items" :fields="fields"></b-table>
+        <b-table striped hover :items="items" :fields="fields">
+            <template #cell(name)="data">
+                <div v-if="data.item.complete">
+                    <del>{{ data.item.name }}</del>
+                </div>
+                <div v-else>
+                    {{ data.item.name }}
+                </div>
+            </template>
+            <template #cell(complete)="data">
+                <div class="row">
+                    <div v-if="!data.item.complete" class="col-6">
+                        <form
+                            method="post"
+                            name="update-task-{{ data.item.id }}"
+                            id="update-task-{{ data.item.id }}"
+                            :action="fetchRoute('tasks.update', {task: data.item.id})"
+                            class="col-6"
+                        >
+                            <input type="hidden" name="_token" :value="csrf">
+                            <input type="hidden" name="_method" value="patch">
+                            <button type="submit" class="btn btn-success">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                                    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+                                </svg>
+                            </button>
+                        </form>
+                    </div>
+                    <div v-if="!data.item.complete" class="col-6">
+                        <form
+                            method="post"
+                            name="delete-task-{{ data.item.id }}"
+                            id="delete-task-{{ data.item.id }}"
+                            :action="fetchRoute('tasks.destroy', {task: data.item.id})"
+                            class="col-6"
+                        >
+                            <input type="hidden" name="_token" :value="csrf">
+                            <input type="hidden" name="_method" value="delete">
+                            <button type="submit" class="btn btn-danger">&times;</button>
+                        </form>
+                    </div>
+                </div>
+            </template>
+        </b-table>
         <b-pagination :per-page="per_page" v-model="current_page" :total-rows="total_rows"></b-pagination>
     </div>
 </template>
@@ -10,6 +53,7 @@ export default {
     name: 'Tasks Table',
     data() {
         return {
+            csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
             current_page: 1,
             last_page: 1,
             per_page: 15,
@@ -38,6 +82,11 @@ export default {
                     this.last_page = data.results.last_page;
                     this.total_rows = data.results.total;
                 });
+        },
+        fetchRoute(routename, param) {
+            console.log(routename);
+            console.log(param);
+            return route(routename, param);
         }
     },
     watch: {
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,64 +16,7 @@
             </div>
             <div class="col-8">
                 <tasks-table></tasks-table>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <td>#</td>
-                            <td>Task</td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                    @foreach($tasks as $task)
-                        <tr>
-                            <td>
-                                {{ $task->id }}
-                            </td>
-                            <td>
-                                @if($task->getCompleteStatus())<del>@endif{{ $task->name }}@if($task->getCompleteStatus())</del>@endif
-                            </td>
-                            <td>
-                                @if(!$task->getCompleteStatus())
-                                    <form
-                                        method="post"
-                                        name="update-task-{{ $task->id }}"
-                                        id="update-task-{{ $task->id }}"
-                                        action="{{route('tasks.update', $task)}}"
-                                        class="col-6"
-                                    >
-                                        @csrf
-                                        @method('PATCH')
-                                        <button type="submit" class="btn btn-success">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-                                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
-                                            </svg>
-                                        </button>
-                                    </form>
-                                @endif
-                            </td>
-                            <td>
-                                @if(!$task->getCompleteStatus())
-                                    <form
-                                        method="post"
-                                        name="delete-task-{{ $task->id }}"
-                                        id="delete-task-{{ $task->id }}"
-                                        action="{{route('tasks.destroy', $task)}}"
-                                        class="col-6"
-                                    >
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">&times;</button>
-                                    </form>
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div><!-- php echo $task->complete ? 'Uncomplete' : 'Complete'; endphp -->
-                </div>
-            </main>
-        </x-layout>
+            </div>
+        </div>
+    </main>
+</x-layout>
